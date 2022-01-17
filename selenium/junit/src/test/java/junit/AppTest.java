86ca910b4ce31a3168a0af2c886be124f0c75c03
jurley.ribeiro <jurley76@hotmail.com>
2022-01-16 21:15:55 -0300

    Add WebDriver Manager

--- selenium/junit/src/test/java/junit/AppTest.java
+++ selenium/junit/src/test/java/junit/AppTest.java
@@ -1,12 +1,12 @@
 /**
  * VVS by Rodrigo Prestes Machado
- * 
  * VVS is licensed under a
  * Creative Commons Attribution 4.0 International License.
  * You should have received a copy of the license along with this
  * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
  *
 */
+
 package junit;
 
 import org.junit.jupiter.api.Test;
--- selenium/junit/src/test/java/junit/WebTest.java
+++ selenium/junit/src/test/java/junit/WebTest.java
@@ -1,23 +1,24 @@
 /**
  * VVS by Rodrigo Prestes Machado
- * 
  * VVS is licensed under a
  * Creative Commons Attribution 4.0 International License.
  * You should have received a copy of the license along with this
  * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
  *
 */
+
 package junit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Exemplo Selenium/Chrome Driver com Junit.
@@ -27,20 +28,33 @@ public class WebTest {
 
     @BeforeAll
     public static void setUp() {
-        WebTest.driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        //Rodar no Linux: Usar os arguments no-sandbox
+        //options.addArguments("--no-sandbox"); // Bypass OS security model
+        //options.addArguments("- disable-dev-shm-usage"); // overcome limited resource problems
+
+        WebTest.driver = new ChromeDriver(options);
     }
 
     @AfterAll
     public static void tearDown() {
-        driver.quit();
+
+        if (WebTest.driver != null) {
+
+            WebTest.driver.quit();
+            WebTest.driver = null;
+        }
+
     }
 
     @Test
     @SuppressWarnings("checkstyle:magicnumber")
     public void web() {
+
         WebTest.driver.get("https://ifrs.edu.br/");
-        WebTest.driver.manage().window().setSize(new Dimension(1440, 877));
-        WebTest.driver.findElement(By.linkText("Editais")).click();
+        WebTest.driver.findElement(By.xpath("//li[@id='menu-item-844']/a")).click();
 
         List<WebElement> elements = WebTest.driver.findElements(By.cssSelector(".editais__title"));
         assert elements.size() > 0;
